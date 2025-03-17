8b07628b93a428b7889dfcff7cb45e9eef507694
Oleksii <popovalex425@gmail.com>
2025-03-17 22:03:10 +0200

    Added public FriendList

--- FriendList.java
+++ FriendList.java
@@ -2,6 +2,32 @@
 // It should use Set<User> hashSet<User> data structure to store the list of friends
 // Methods: addFriend(User u), removeFriend(User u), displayFriends(), getFriend(), isFriend(User u) - check if the friend in a friendlist
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FriendList {
+    private Set<User> friends;
+    private HashSet<User> hashSet;
     
+    static void addFriend(User u) {
+        // add friend to the list
+    }
+
+    static void removeFriend(User u) {
+        // remove friend from the list
+    }
+
+    static void displayFriends() {
+        // display all friends
+    }
+
+    static User getFriend() {
+        // get a friend
+        return null;
+    }
+
+    static boolean isFriend(User u) {
+        // check if the friend in a friendlist
+        return false;
+    }
 }
